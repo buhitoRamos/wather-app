c61822870245c82e67cbcc93078ed7c2720fc675
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-10-18 20:23:04 -0300

    feat(*): first commit

---
+++ src/components/CityInfo/CityInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import 'typeface-roboto'
+import  Typography  from '@material-ui/core/Typography'
+
+const CityInfo = ({city, country}) => {
+    return (
+        <>
+            
+             <Typography variant='h4' display='inline'> {city}, </Typography>
+             <Typography variant='h6' display='inline'>{country}</Typography>
+           
+        </>
+    )
+}
+
+CityInfo.propTypes = {
+    city: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired
+
+}
+
+export default CityInfo
